dbada87b5c519106a27e0db50169509f9f2e76c6
Matthew Shin <mshin1995@gmail.com>
2019-02-20 03:16:44 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -55,5 +55,15 @@ def find_cool
 end
 
 def organize_schools
-  
+  organized_schools = {}
+  schools.each do |name, location_hash|
+    location = location_hash[:location]
+    if organized_schools[location]
+      organized_schools[location] << name
+    else
+      organized_schools[location] = []
+      organized_schools[location] << name
+    end
+  end
+  organized_schools
 end
