810a2ad623563138ca3e7379e4e4e4131faef60e
Matthew Shin <mshin1995@gmail.com>
2019-02-20 03:14:00 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -23,7 +23,15 @@ def remove_non_strings(array)
 end
 
 def count_elements(array)
-  
+  array.each do |original_hash|
+    original_hash[:count] = 0
+    name = original_hash[:name]
+    array.each do |hash|
+      if hash[:name] == name
+        original_hash[:count] += 1
+      end
+    end
+  end.uniq
 end
 
 def merge_data
