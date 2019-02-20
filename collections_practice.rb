7d7e27a7c728ee6b44d77e4cf9150eb86eff6b50
Matthew Shin <mshin1995@gmail.com>
2019-02-20 03:14:16 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -35,7 +35,15 @@ def count_elements(array)
 end
 
 def merge_data
-  
+  arr2[0].map do |name, prop_hash|
+    new_prop_hash = {}
+    arr1.each do |new_attr_hash|
+      if new_attr_hash[:first_name] == name
+        new_prop_hash = prop_hash.merge(new_attr_hash)
+      end
+    end
+    new_prop_hash
+  end
 end
 
 def find_cool
